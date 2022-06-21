f6f984540ac77caf6d4e3b8bf74f0198dbf8883e
h-replica <67347771+h-replica@users.noreply.github.com>
2022-06-22 01:16:10 +0530

    Time: 921 ms (38.60%), Space: 28.6 MB (54.31%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -1,5 +1,11 @@
 class Solution:
     def furthestBuilding(self, h: List[int], bricks: int, ladders: int) -> int:
+        
+        """
+            Go greedy instead of DP (constraints are too high for dp)
+            
+            leave larger climb for ladders and use bricks for minimum diff heights
+        """
         heap=[]
         heapify(heap)
         for i in range(len(h)-1):
