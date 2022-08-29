c0553b8e6240488223c2dda23e3a587ec7b2f113
h-replica <67347771+h-replica@users.noreply.github.com>
2022-08-30 01:38:21 +0530

    Time: 1226 ms (83.58%), Space: 48.4 MB (83.84%) - LeetHub

---
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
@@ -0,0 +1,62 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        size = 0
+        curr = head 
+        while(curr):
+            size+=1
+            curr= curr.next
+        
+        if size <= 1 :
+            return head
+        
+        left = min(k , size-k+1)
+        right = max(k , size-k+1)
+        
+        prev1 = None
+        curr1 = head
+        next1 = curr1.next
+        
+        
+        i = left
+        while(i > 1) :
+            prev1 = curr1 
+            curr1 = next1
+            next1 = next1.next
+            i-=1
+            
+        
+        prev2 = None
+        curr2 = head
+        next2  =curr2.next
+        
+        i = right
+        while(i > 1) :
+            prev2 = curr2 
+            curr2 = next2
+            next2 = next2.next
+            i-=1
+        
+        
+        # print(prev1 ,"|" ,  curr1 ,"|", next1 , "|",prev2 , "|",curr2 ,"|", next2 )
+        
+        if prev1 : prev1.next = curr2
+        if prev2 : 
+            if prev2 == curr1 :
+                curr2.next = curr1
+                curr1.next = next2
+            else:    
+                prev2.next = curr1
+                curr1.next = next2
+                curr2.next = next1
+        
+        return curr2 if not prev1 else head
+        return head
+        
+            
+            
